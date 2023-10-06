1cdf0fc9ba06f4ba9481178cb91aa8a077106a2c
v4roah1994 <64238831+V4roah@users.noreply.github.com>
2023-10-06 14:49:44 -0500

    Merge clase13

--- main.js
+++ main.js
@@ -4,15 +4,42 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const menuCarIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
 
 
 menuEmail.addEventListener('click', () => {
+    const isCarIconOpen = !aside.classList.contains('inactive')
+    if(isCarIconOpen){
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
   });
 
-  menuHamIcon.addEventListener('click', () => {
+menuHamIcon.addEventListener('click', () => {
+    const isCarIconOpen = !aside.classList.contains('inactive')
+    if(isCarIconOpen){
+        aside.classList.add('inactive')
+    }
+
     mobileMenu.classList.toggle('inactive')
-  
+})
+
+menuCarIcon.addEventListener('click', () => {
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive')
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive')
+
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactive')
+    }
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive')
+
+    }
+
+    aside.classList.toggle('inactive')
 
 })
 
